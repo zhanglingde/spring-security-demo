a4f50d22d07ac44c9c171ec51aad5ea90540bcbd
zhangling <zhangling2@maycur.com>
2021-09-13 18:04:07 +0800

    表单登录

---
+++ chapter02/formlogin/src/main/java/com/ling/formlogin/FormloginApplication.java
@@ -0,0 +1,13 @@
+package com.ling.formlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormloginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormloginApplication.class, args);
+    }
+
+}
---
+++ chapter02/formlogin/src/test/java/com/ling/formlogin/FormloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ling.formlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormloginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
