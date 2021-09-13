7d9555eebbacf73a162a54897c212416e10c1aa9
zhangling <zhangling2@maycur.com>
2021-09-13 17:54:31 +0800

    Spring Security 基本认证

---
+++ chapter02/firstdemo/src/main/java/com/ling/firstdemo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.ling.firstdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello spring security";
+    }
+}
